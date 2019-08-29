0c88f4b412abcea9852079155fc7db3495f603d6
Michał <michal.warda@elpassion.pl>
2019-08-29 10:15:31 +0200

    Optimize tests

--- src/test/kotlin/pl/elpassion/demo2/Demo2ApplicationTests.kt
+++ src/test/kotlin/pl/elpassion/demo2/Demo2ApplicationTests.kt
@@ -1,47 +1,43 @@
 package pl.elpassion.demo2
 
-import org.assertj.core.api.Assertions
+import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+
 
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @ActiveProfiles("test")
+@AutoConfigureMockMvc
 class Demo2ApplicationTests {
 
     @Autowired
-    private val restTemplate: TestRestTemplate? = null
+    private val mockMvc: MockMvc? = null
+
+    val expected = "[{\"id\":1,\"firstName\":\"Kewin\",\"lastName\":\"Czupryński\"}]"
 
     @Test
     fun contextLoads() {
-        val body = this.restTemplate?.getForObject("/", Array<Customer>::class.java)
-        body as Array<Customer>
-        Assertions
-                .assertThat(body)
-                .isEqualTo(arrayOf(Customer().apply {
-                    id = 1
-                    firstName = "Kewin"
-                    lastName = "Czupryński"
-                }))
+        mockMvc!!.perform(get("/")).andReturn().also {
+            Assert.assertEquals(it.response.contentAsString, expected)
+        }
     }
 
     @Test
     fun contextLoads2() {
-        val body = this.restTemplate?.getForObject("/", Array<Customer>::class.java)
-        body as Array<Customer>
-        Assertions
-                .assertThat(body)
-                .isEqualTo(arrayOf(Customer().apply {
-                    id = 1
-                    firstName = "Kewin"
-                    lastName = "Czupryński"
-                }))
+        mockMvc!!.perform(get("/")).andReturn().also {
+            Assert.assertEquals(it.response.contentAsString, expected)
+        }
     }
 }
+
+
